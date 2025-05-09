fcd341e946d888cf59e12724b2ade9c51b727082
Rajan Verma <111996500+boylish@users.noreply.github.com>
2025-05-09 16:52:56 +0530

    Update Page3.jsx

--- src/components/pages/Page3.jsx
+++ src/components/pages/Page3.jsx
@@ -7,16 +7,16 @@ import { FaArrowRight } from "react-icons/fa";
 const Page3 = () => {
   return (
     <div className="w-full py-8 md:py-30 px-4 min-h-screen border-t-2 bg-gradient-to-b from-[#f4efe7] to-white flex flex-col justify-evenly">
-      {/* Section Title */}
+      
       <div className="flex items-center justify-center mb-8 md:mb-12">
         <div className="p-2 border border-gray-500">
           <h2 className="text-2xl md:text-4xl font-bold p-4">II</h2>
         </div>
       </div>
 
-      {/* Image Cards */}
+
       <div className="flex flex-wrap justify-center gap-6 md:gap-8 lg:justify-between">
-        {/* Card 1 */}
+    
         <div className="relative w-full sm:w-[45%] md:w-[30%] lg:w-[30%] xl:w-[30%] aspect-square overflow-hidden group">
           <img
             src={hotel_room}
@@ -29,7 +29,7 @@ const Page3 = () => {
           </h1>
         </div>
 
-        {/* Card 2 */}
+
         <div className="relative w-full sm:w-[45%] md:w-[30%] lg:w-[30%] xl:w-[30%] aspect-square overflow-hidden group">
           <img
             src={hotel3}
@@ -42,7 +42,7 @@ const Page3 = () => {
           </h1>
         </div>
 
-        {/* Card 3 */}
+
         <div className="relative w-full sm:w-[45%] md:w-[30%] lg:w-[30%] xl:w-[30%] aspect-square overflow-hidden group">
           <img
             src={hotel_dining}
@@ -56,7 +56,7 @@ const Page3 = () => {
         </div>
       </div>
 
-      {/* Button */}
+ 
       <div className="flex justify-center items-center mt-6 md:mt-8">
         <div className="group relative inline-flex items-center justify-center border-2 border-blue-900 rounded-full px-6 py-3 md:px-10 md:py-5 transition-all duration-500 ease-in-out overflow-hidden hover:w-16 hover:h-16 hover:px-0 hover:py-0 hover:bg-blue-900">
           {/* Text - Hidden on hover */}
@@ -64,7 +64,7 @@ const Page3 = () => {
             Accomodation
           </span>
 
-          {/* Arrow Icon - Visible on hover */}
+     
           <FaArrowRight className="absolute text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out" />
         </div>
       </div>
